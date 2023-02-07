c40ad82cc120a4525a29d049055de5d835fd7593
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-07 15:29:02 -0500

    fetch info useEffect

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+useEffect(() => {
+ fetch ("http://localhost:3001/stocks")
+  .then (res => res.json())
+  .then (data => console.log(data))
+}, [])
+
   return (
     <div>
       <h2>Stocks</h2>
+      <Stock />
       {/* render stock list here*/}
     </div>
   );
